64886aee4df1204338ceb56409ed64bbb984d792
Pushhpendra Kumar <113991886+pushpendrakumar1@users.noreply.github.com>
2023-09-21 16:44:06 +0530

    Delete Contact_Management directory

--- Contact_Management/contact_mang/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ContactMangConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contact_mang'
--- Contact_Management/contact_mang/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    number = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.nam
